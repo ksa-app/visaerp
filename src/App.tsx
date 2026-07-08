1a34829777dd13c8a03204fd352523c3eaebc3f7
seven <sevenstar2202@gmail.com>
2026-07-08 18:11:43 +0600

    try supabase connect and auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import SingUp from './pages/SignUp'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-xl'>Hello World</h1>
+      <SingUp />
     </>
   )
 }
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,16 @@
+export default function SingUp(){
+    return(
+        <>
+            <h1>SingUp</h1>
+            <input 
+                type="email"
+                className=""
+            />
+            <input 
+                type="password"
+                className=""
+            />
+            <button>SingUp</button>
+        </>
+    );
+}
